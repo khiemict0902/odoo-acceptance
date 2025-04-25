5564905d1e1f58187760fc04691ab7b4bf22214d
khiemict0902 <khiem0902@gmail.com>
2025-04-25 10:09:52 +0700

    sửa theo yêu cầu

--- project_performance/models/inherit_task.py
+++ project_performance/models/inherit_task.py
@@ -1,7 +1,28 @@
 from odoo import api, fields, models
+from odoo.exceptions import UserError
 
 class InheritTask(models.Model):
     _inherit = 'project.task'
 
     pr_ids = fields.One2many('prj.pull.request','task_id','Pull Requests')
     is_update = fields.Boolean(default=False)
+    is_close = fields.Boolean(default=False)
+
+    def action_rollback_code(self):
+        for task in self:
+            if task.is_update:
+                for pr in task.pr_ids:
+                    if pr.prev_state:
+                        pr.state = pr.prev_state
+                task.is_update = False
+
+                plans = self.env['prj.update.plan'].search([
+                    ('task_ids', 'in', task.id),
+                    ('state', 'in', ['done'])
+                ])
+                for plan in plans:
+                    plan.state = 'approved'
+
+
+            else:
+                raise UserError("Nhiệm vụ này chưa được cập nhật, không cần rollback.")
